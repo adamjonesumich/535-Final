52b992504d930ba78292809882b87254384bfc19
adamjnes <adamjnes@umich.edu>
2024-12-11 19:25:47 -0500

    second commit

---
+++ Rocket.m
@@ -0,0 +1,70 @@
+classdef Rocket
+    %RocketParameters Container to pass variables between functions
+    %   Detailed explanation goes here
+
+    properties
+        % -------------inputs--------------
+        % mission parameters - nonadjustable
+        total_delta_v
+
+        % architecture dependent - basically nonadjustable
+        number_stages
+        fuel_species
+        oxidizer_species
+        average_ambient_pressure % could change to be a vector/range; also 
+        % could change to be calculated somehow, or be a product of thrust
+
+        % adjustable
+        mass_fuel_ratio
+        area_throat
+        chamber_pressure
+        % ----------------------------------
+        
+        % --------cantera outputs-----------
+        chamber_temperature
+        mixture_molecular_weight
+        mixture_gamma
+        % ----------------------------------
+        
+        % --------quasi-1D outputs----------
+        area_exit_ratio
+        mass_flow_rate
+        exit_mach_number
+        specific_impulse
+        % ----------------------------------
+
+        % ------multistaging outputs--------
+        stage_structural_coefficients
+        stage_mass_ratios
+        stage_masses
+    end
+
+    methods
+        function obj = Rocket(total_delta_v,number_stages,fuel_species,oxidizer_species,mass_fuel_ratio,area_throat,average_ambient_pressure,chamber_pressure)
+            %Rocket Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.total_delta_v = total_delta_v;
+            obj.number_stages = number_stages;
+            obj.fuel_species = fuel_species;
+            obj.oxidizer_species = oxidizer_species;
+            obj.mass_fuel_ratio = mass_fuel_ratio;
+            obj.area_throat = area_throat;
+            obj.average_ambient_pressure = average_ambient_pressure;
+            obj.chamber_pressure = chamber_pressure;
+
+            obj.chamber_temperature = NaN;
+            obj.mixture_molecular_weight = NaN;
+            obj.mixture_gamma = NaN;
+    
+            obj.area_exit_ratio = NaN;
+            obj.mass_flow_rate = NaN;
+            obj.exit_mach_number = NaN;
+            obj.specific_impulse = NaN;
+            
+            obj.stage_structural_coefficients = NaN;
+            obj.stage_mass_ratios = NaN;
+            obj.stage_masses = NaN;
+        end
+
+    end
+end
---
+++ main.m
@@ -0,0 +1,53 @@
+% initialize
+
+total_delta_v = 8000; % m/s
+number_stages = 3; 
+fuel_species = "CH4";
+oxidizer_species = "O2";
+mass_fuel_ratio = 4;
+area_throat = 0.05; % m2
+average_ambient_pressure = 0; % Pa
+chamber_pressure = 5 * 1e6;% Pa
+
+rocket = Rocket(total_delta_v,number_stages,fuel_species,oxidizer_species, ...
+    mass_fuel_ratio,area_throat,average_ambient_pressure,chamber_pressure);
+
+rocket = cantera(rocket);
+rocket = quasi_1d(rocket);
+rocket = nozzle_adjust(rocket);
+rocket = multistage(rocket);
+
+
+function rocket = cantera(rocket)
+    % valid inputs: chamber_pressure, fuel_species, oxidizer_species,
+    % mass_fuel_ratio
+
+    % expected outputs: chamber_temperature, mixture_gamma, 
+    % mixture_molecular_weight
+end
+
+
+function rocket = quasi_1d(rocket)
+    % valid inputs: chamber_temperature, chamber_pressure, 
+    % average_ambient_pressure, mixture_gamma, mixture_molecular_weight,
+    % area_throat
+
+    % expected outputs: area_exit_ratio, mass_flow_rate, exit_mach_number,
+    % preliminary specific_impulse
+end
+
+function rocket = nozzle_adjust(rocket)
+    % valid inputs: all thermodynamic variables
+
+    % expected output: adjusted specific_impulse
+
+    % note: this is a combination of Gamba's 2D nozzle solver, the "nozzle
+    % chopper", and the Isp adjuster script
+end
+
+function rocket = multistage(rocket)
+    % valid inputs: all thermodynamic variables
+
+    % expected output: all structural variables
+end
+
