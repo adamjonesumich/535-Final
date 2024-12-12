fa44f441ac203d9bd50437793e812bf82fd28744
adamjnes <adamjnes@umich.edu>
2024-12-11 20:42:40 -0500

    comments

--- main.m
+++ main.m
@@ -10,16 +10,18 @@ area_throat = [0.05; 0.05; 0.05; 0.05]; % m2
 average_ambient_pressure = [101325; 100; 0; 0]; % Pa
 chamber_pressure = [5; 5.5; 6; 6] * 1e6; % Pa
 
+% struct to hold data between function calls
 rocket = Rocket(total_delta_v,upper_stage_mass,number_launch_stages,fuel_species,oxidizer_species, ...
     mass_fuel_ratio,area_throat,average_ambient_pressure,chamber_pressure);
 
-% solve
+% solve!
 
 rocket = cantera(rocket);
-% for testing purposes
-rocket.chamber_temperature = [1500; 1600; 1700; 1700];
-rocket.mixture_gamma = [1.2; 1.15; 1.1; 1.1];
-rocket.mixture_molecular_weight = [18; 17; 16; 16];
+
+    % for testing purposes: fake cantera!
+    rocket.chamber_temperature = [1500; 1600; 1700; 1700];
+    rocket.mixture_gamma = [1.2; 1.15; 1.1; 1.1];
+    rocket.mixture_molecular_weight = [18; 17; 16; 16];
 
 rocket = quasi_1d(rocket);
 rocket = nozzle_adjust(rocket);
